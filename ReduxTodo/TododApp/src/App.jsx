eec88efa2a7cd172ffb5cbded41683d2ab9f59dd
chaitanya <g.chaitanya1419@gmail.com>
2023-12-12 12:58:20 +0530

    changed

--- ReduxTodo/TododApp/src/App.jsx
+++ ReduxTodo/TododApp/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Carousel from 'react-bootstrap/Carousel';
+
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+const App = () => {
+ return(
+  <div class="container-fluid">
+  <div class="row">
+    <div class="col-6">
+      <h1 className=''>Taj Mahal</h1>
+      <p>
+        The Taj Mahal is an ivory-white marble mausoleum on the southern
+        bank of the river Yamuna in the Indian city of Agra.
       </p>
-    </>
-  )
+    </div>
+    <div class="col-6">
+      <h1>Taj Mahal</h1>
+      <p>
+        The Taj Mahal is an ivory-white marble mausoleum on the southern
+        bank of the river Yamuna in the Indian city of Agra.
+      </p>
+    </div>
+  </div>
+</div>
+
+  
+ )
+  
+
 }
 
 export default App
