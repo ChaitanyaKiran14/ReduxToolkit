f9455cfaf22e39d0abe69571064f7433d8d3dae7
chaitanya <g.chaitanya1419@gmail.com>
2023-12-14 12:26:28 +0530

    updated

--- ReduxEcomerce/Ecom/src/components/ProductItem/ProductItem.jsx
+++ ReduxEcomerce/Ecom/src/components/ProductItem/ProductItem.jsx
@@ -2,23 +2,24 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './index.css'
 
-const ProductItem = () => {
+const ProductItem = (props) => {
+  const {product} = props
+  const { id, image, price, title } = product;
 
+  return (
+    
+      <Card className='random' key={id} style={{ width: '18rem' }}>
+          <Card.Img className='imgz' variant="top" src={image} />
+          <Card.Body>
+              <Card.Title>{title}</Card.Title>
+              <Card.Text> INR : {price}</Card.Text>
+              
+          </Card.Body>
+          <Card.Footer style={{backgroundColor : 'white'}}>
+          <Button variant="primary">Add To Cart</Button>
 
-    return (
-        <Card style={{ width: '18rem' }}>
-      <Card.Img  className='imgz' variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSplwqFoER38QniLdVAJJYIZ6KPP7tA2N8kyAwGpIQgXzAm84hY2IHpwav5vF1PkSs4s0o&usqp=CAU" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Add To Cart</Button>
-      </Card.Body>
-    </Card>
-    )
-
+          </Card.Footer>
+      </Card>
+  );
 }
-
-export default ProductItem
+ export default ProductItem
--- ReduxEcomerce/Ecom/src/components/Products/Products.jsx
+++ ReduxEcomerce/Ecom/src/components/Products/Products.jsx
@@ -1,10 +1,43 @@
 import ProductItem from "../ProductItem/ProductItem"
+import { useEffect, useState, useCallback } from 'react';
 const Products = () => {
+
+    const [products, setProducts] = useState([])
+
+    const getProducts  = useCallback(async  () => {
+        const url = "https://fakestoreapi.com/products"
+        const options = {
+            method: "GET",
+        }
+        const response = await fetch(url, options)
+        const data =  await response.json()
+
+        const newData = data.map((each) => ({
+            id : each.id,
+            title : each.title,
+            image : each.image,
+            price : each.price
+        }))
+        setProducts(newData)
+        console.log(newData)
+
+    }, [] )
+
+    useEffect(() => {
+        getProducts()
+    }, [getProducts])
 
 
     return(
-        <div>
-            <ProductItem/>
+        <div className="container">
+            <h2>All products</h2>
+            <div className="row">
+                {products.map((each) => (
+                    <div key={each.id} className="col-md-3">
+                        <ProductItem product={each} />
+                    </div>
+                ))}
+            </div>
         </div>
     )
 
